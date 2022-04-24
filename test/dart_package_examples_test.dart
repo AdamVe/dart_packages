2ccaf727d26bec09f9c776d1d6507cbfbc400951
Adam Velebil <pardalek@gmail.com>
2022-04-24 10:01:39 +0200

    initial

---
+++ test/dart_package_examples_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_package_examples/dart_package_examples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
