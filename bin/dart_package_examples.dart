a58788d68a103bb9c521ce6dde1c3a86d14c84f9
Adam Velebil <pardalek@gmail.com>
2022-04-24 10:32:46 +0200

    added git_based package

--- bin/dart_package_examples.dart
+++ bin/dart_package_examples.dart
@@ -1,5 +1,7 @@
+import 'package:git_based/git_based.dart' as git_based;
 import 'package:local/local.dart' as local;
 
 void main() {
   print('Result from local: ${local.calculate()}!');
+  print('Result from git_based: ${git_based.calculate()}!');
 }
